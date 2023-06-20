25f12645b50445e851d47bbc67fda95e4cdd1c61
Elton Santos <eltonaxl@hotmail.com>
2023-06-19 23:37:02 -0300

    Class core

---
+++ src/core/Client.ts
@@ -0,0 +1,27 @@
+export default class Client {
+  private id: string | null
+  private name: string
+  private age: number
+
+  constructor(name: string, age: number, id: null) {
+    this.name = name
+    this.age = age
+    this.id = id
+  }
+
+  static empty() {
+    return new Client('', 0, null)
+  }
+
+  get getId() {
+    return this.id
+  }
+
+  get getAge() {
+    return this.age
+  }
+
+  get getName() {
+    return this.name
+  }
+}
