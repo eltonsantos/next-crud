a75e6c7eddd5955f74d45cd3640b8a3dad6b292b
Elton Santos <eltonaxl@hotmail.com>
2023-06-19 23:29:41 -0300

    Component Layout and Title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import Layout from "@/components/Layout";
+
 export default function Home() {
   return (
-    <div className="flex h-screen justify-center items-center">
-      Teste
+    <div className="flex h-screen justify-center items-center bg-gradient-to-r from-blue-500 to-purple-500 text-white">
+      <Layout title="Cadastro simples">
+        <span>Conteúdo</span>
+      </Layout>
     </div>
   )
 }
